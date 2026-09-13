c28531cfe1d62282b8eb9480c28a152db1a23f24
lefernandezc <le.fernandezc@duocuc.cl>
2026-09-13 15:33:52 -0300

    iniciado el proyecto

---
+++ appointments/src/test/java/appointments/ApplicationTests.java
@@ -0,0 +1,13 @@
+package appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
